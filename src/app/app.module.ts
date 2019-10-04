03de9ccde464420558b226e4e5710c896f3c9878
V1S1TER-PC\V1s1ter <lukin@fastmail.com>
2019-10-04 10:24:40 +0300

    version 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,29 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CalcButtonComponent } from './components/calc-button/calc-button.component';
+import { CalcExpressionComponent } from './components/calc-expression/calc-expression.component';
+import {FormsModule} from '@angular/forms';
+import { CalcResultComponent } from './components/calc-result/calc-result.component';
+import { CalcHistoryListComponent } from './components/calc-history-list/calc-history-list.component';
+import { CalcHistoryItemComponent } from './components/calc-history-item/calc-history-item.component';
+import { CalcButtonUnitComponent } from './components/calc-button-unit/calc-button-unit.component';
+import { CalcComponent } from './components/calc/calc.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CalcButtonComponent,
+    CalcExpressionComponent,
+    CalcResultComponent,
+    CalcHistoryListComponent,
+    CalcHistoryItemComponent,
+    CalcButtonUnitComponent,
+    CalcComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
